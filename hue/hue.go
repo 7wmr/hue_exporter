4a187dca900344d0dbaa7427486ca1d26c400405
Richard Neave <4736295+7wmr@users.noreply.github.com>
2021-09-14 13:35:54 +0100

    Update hue.go

--- hue/hue.go
+++ hue/hue.go
@@ -203,7 +203,7 @@ func collectLights(bridge *hueAPI.Bridge) (lightData []map[string]interface{}, e
 		// State
 		result[labelStateOn] = light.State.On
 		result[labelStateAlert] = light.State.Alert
-		result[labelStateBri] = light.State.Bri + "222"
+		result[labelStateBri] = light.State.Bri / 100
 		result[labelStateColorMode] = light.State.ColorMode
 		result[labelStateCT] = light.State.Ct
 		result[labelStateReachable] = light.State.Reachable
